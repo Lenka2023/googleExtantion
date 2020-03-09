433354dc638e626e60e46388c716e2bbe044e03b
Lenka2023 <lenka20231@gmail.com>
2020-03-09 14:18:05 +0200

    fix mistake

--- options.js
+++ options.js
@@ -5,7 +5,7 @@
 'use strict';
 
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-
+ let page = document.getElementById('buttonDiv');
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
